af2260aa3841c7945eedd1222eae7699c26ffad0
danielvalmeida91 <danielvalmeida91@gmail.com>
2024-11-23 13:18:22 -0300

    config: finish initial config

--- back/eslint.config.js
+++ back/eslint.config.js
@@ -1,33 +1,35 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.{js,mjs,cjs,ts,tsx}"]},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
-      "semi": "off",
+      "semi": ["error", "never"],
+      "quotes": ["error", "double"],
       "eqeqeq": "error",
       "no-console": "warn",
-      "no-unused-expressions": "warn",
       "no-unused-vars": "warn",
-      "no-useless-concat": "warn",
       "no-useless-return": "warn",
       "no-useless-rename": "warn",
       "no-var": "error",
-      "no-return-await":"error",
-      "no-await-in-loop":"warn",
+      "no-return-await": "error",
+      "no-await-in-loop": "warn",
       "no-mixed-spaces-and-tabs": "error",
       "require-await": "error",
       "indent": ["error", 2],
-      "space-before-function-paren": ["error", "always"],
+      "space-before-function-paren": ["error", "never"],
       "space-in-parens": ["error", "never"],
-      "space-infix-ops":"error"
+      "space-infix-ops": "error",
+      "no-duplicate-imports": "error",
+      "prefer-const": "error",
+      "eol-last": ["error", "always"]
     }
-  },
-];
+  }
+]
--- back/src/app.ts
+++ back/src/app.ts
@@ -1,3 +1,23 @@
-import fastify from 'fastify';
+import { PrismaClient } from "@prisma/client"
+import fastify from "fastify"
 
-export const app = fastify();
+export const app = fastify()
+
+const prisma = new PrismaClient()
+
+function teste() {
+  return "teste"
+}
+
+teste()
+
+prisma.user.create({
+  data:{
+    name: "Daniel",
+    email: "daniel@gmail.com"
+  }
+})
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World"})
+})
